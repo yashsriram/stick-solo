7abbb63c0975ada215e19654d3efce42ba58484f
Yashasvi Sriram <yash.3997@gmail.com>
2020-05-02 23:07:55 -0500

    impls N revolute joint agent hopping on milestones of a path generated by PRM, impls NRIterativeAgent{}, NRIterativeAgentOnPRM{}

---
+++ src/NRIterativeAgentOnPRM.java
@@ -0,0 +1,116 @@
+import camera.QueasyCam;
+import math.Vec;
+import processing.core.PApplet;
+import robot.acting.NRIterativeAgent;
+import robot.planning.prm.PRM;
+
+import java.util.List;
+
+public class NRIterativeAgentOnPRM extends PApplet {
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 800;
+    private static final int SIZE = 100;
+    private static String SEARCH_ALGORITHM = "";
+
+    private static final Vec MIN_CORNER = new Vec(-SIZE, -SIZE);
+    private static final Vec MAX_CORNER = new Vec(SIZE, SIZE);
+    private static final Vec START_POSITION = new Vec(SIZE * (-9f / 10), SIZE * (9f / 10));
+    private static final Vec GOAL_POSITION = new Vec(SIZE * (9f / 10), SIZE * (-9f / 10));
+    private static final float L1 = 5;
+    private static final float L2 = 10;
+    private static final float L3 = 5;
+    private static final float L4 = 10;
+    private static final float MAX_EDGE_LEN = L1 + L2 + L3 + L4 - 1;
+    private static final float MIN_EDGE_LEN = 0;
+    private static final int NUM_MILESTONES = 200;
+
+    QueasyCam cam;
+    PRM prm;
+    NRIterativeAgent nrIterativeAgent;
+
+    public void settings() {
+        size(WIDTH, HEIGHT, P3D);
+    }
+
+    public void setup() {
+        surface.setTitle("Processing");
+        colorMode(RGB, 1.0f);
+        rectMode(CENTER);
+        noStroke();
+
+        cam = new QueasyCam(this);
+        nrIterativeAgent = new NRIterativeAgent(this, 4);
+        prm = new PRM(this);
+        int numEdges = prm.grow(NUM_MILESTONES, MIN_CORNER, MAX_CORNER, MIN_EDGE_LEN, MAX_EDGE_LEN);
+        PApplet.println("# milestones : " + NUM_MILESTONES + " # edges : " + numEdges);
+    }
+
+    public void draw() {
+        // Reset
+        background(0);
+
+        // Update
+        for (int i = 0; i < 2; i++) {
+            nrIterativeAgent.update(0.0001f);
+        }
+
+        // Draw
+        nrIterativeAgent.draw();
+        prm.draw();
+
+        surface.setTitle("Processing:"
+                + " FPS: " + (int) frameRate
+                + " Search: " + SEARCH_ALGORITHM
+        );
+    }
+
+    @Override
+    public void keyPressed() {
+        if (key == 'c') {
+            cam.controllable = !cam.controllable;
+        }
+        if (key == 'p') {
+            nrIterativeAgent.isPaused = !nrIterativeAgent.isPaused;
+        }
+        if (key == 'k') {
+            PRM.DRAW_MILESTONES = !PRM.DRAW_MILESTONES;
+        }
+        if (key == 'j') {
+            PRM.DRAW_EDGES = !PRM.DRAW_EDGES;
+        }
+        if (key == '1') {
+            List<Vec> path = prm.dfs(START_POSITION, GOAL_POSITION, MIN_EDGE_LEN, MAX_EDGE_LEN);
+            nrIterativeAgent.spawn(path, new Vec(L1, L2, L3, L4), new Vec(0, 0, 0, 0));
+            SEARCH_ALGORITHM = "DFS";
+        }
+        if (key == '2') {
+            List<Vec> path = prm.bfs(START_POSITION, GOAL_POSITION, MIN_EDGE_LEN, MAX_EDGE_LEN);
+            nrIterativeAgent.spawn(path, new Vec(L1, L2, L3, L4), new Vec(0, 0, 0, 0));
+            SEARCH_ALGORITHM = "BFS";
+        }
+        if (key == '3') {
+            List<Vec> path = prm.ucs(START_POSITION, GOAL_POSITION, MIN_EDGE_LEN, MAX_EDGE_LEN);
+            nrIterativeAgent.spawn(path, new Vec(L1, L2, L3, L4), new Vec(0, 0, 0, 0));
+            SEARCH_ALGORITHM = "UCS";
+        }
+        if (key == '4') {
+            List<Vec> path = prm.aStar(START_POSITION, GOAL_POSITION, MIN_EDGE_LEN, MAX_EDGE_LEN);
+            nrIterativeAgent.spawn(path, new Vec(L1, L2, L3, L4), new Vec(0, 0, 0, 0));
+            SEARCH_ALGORITHM = "A*";
+        }
+        if (key == '5') {
+            List<Vec> path = prm.weightedAStar(START_POSITION, GOAL_POSITION, MIN_EDGE_LEN, MAX_EDGE_LEN, 1.5f);
+            nrIterativeAgent.spawn(path, new Vec(L1, L2, L3, L4), new Vec(0, 0, 0, 0));
+            SEARCH_ALGORITHM = "weighted A*";
+        }
+    }
+
+    static public void main(String[] passedArgs) {
+        String[] appletArgs = new String[]{"NRIterativeAgentOnPRM"};
+        if (passedArgs != null) {
+            PApplet.main(concat(appletArgs, passedArgs));
+        } else {
+            PApplet.main(appletArgs);
+        }
+    }
+}
---
+++ src/robot/acting/NRIterativeAgent.java
@@ -0,0 +1,161 @@
+package robot.acting;
+
+import math.Angle;
+import math.Vec;
+import processing.core.PApplet;
+import robot.planning.ik.RRIKSolver;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NRIterativeAgent {
+    public static float MILESTONE_REACHED_SLACK = 1f;
+    public static float JERK_THRESHOLD = 1e-6f;
+    public boolean isPaused = false;
+
+    private final PApplet applet;
+
+    private List<Vec> path = new ArrayList<>();
+    private int nextMilestone = 1;
+
+    private final Vec pivotPosition = new Vec(0f, 0f);
+    private final int N;
+    private final Vec lengths;
+    private final Vec jointTuple;
+
+    public NRIterativeAgent(PApplet applet, int N) {
+        this.applet = applet;
+        this.N = N;
+        this.lengths = new Vec(new float[N]);
+        this.jointTuple = new Vec(new float[N]);
+    }
+
+    public void spawn(List<Vec> path, Vec lengths, Vec jointTuple) {
+        if (lengths.getNumElements() != N || jointTuple.getNumElements() != N) {
+            PApplet.println("Invalid spawn parameters");
+            return;
+        }
+        if (path.size() > 0) {
+            Vec firstMilestone = path.get(0);
+            this.pivotPosition.headSet(firstMilestone.get(0), firstMilestone.get(1));
+        } else {
+            this.pivotPosition.headSet(0, 0);
+        }
+        this.lengths.headSet(lengths);
+        this.jointTuple.headSet(jointTuple);
+        this.path = new ArrayList<>(path);
+        this.nextMilestone = 1;
+    }
+
+    private List<Vec> getLinkEnds() {
+        List<Vec> ends = new ArrayList<>(Collections.singletonList(new Vec(pivotPosition)));
+        Vec prevEnd = new Vec(pivotPosition);
+        float sumOfPreviousAngles = 0;
+        for (int i = 0; i < jointTuple.getNumElements(); i++) {
+            sumOfPreviousAngles += jointTuple.get(i);
+            prevEnd.set(0, prevEnd.get(0) + (float) (lengths.get(i) * Math.cos(sumOfPreviousAngles)));
+            prevEnd.set(1, prevEnd.get(1) + (float) (lengths.get(i) * Math.sin(sumOfPreviousAngles)));
+            ends.add(new Vec(prevEnd));
+        }
+        return ends;
+    }
+
+    private Vec getFreeEnd() {
+        Vec freeEnd = new Vec(pivotPosition);
+        float angleWithX = 0;
+        for (int i = 0; i < jointTuple.getNumElements(); i++) {
+            angleWithX += jointTuple.get(i);
+            freeEnd.set(0, freeEnd.get(0) + (float) (lengths.get(i) * Math.cos(angleWithX)));
+            freeEnd.set(1, freeEnd.get(1) + (float) (lengths.get(i) * Math.sin(angleWithX)));
+        }
+        return freeEnd;
+    }
+
+    public void update(float dt) {
+        if (isPaused) {
+            return;
+        }
+        if (nextMilestone < path.size()) {
+            // Reached next milestone
+            if (Vec.dist(getFreeEnd(), path.get(nextMilestone)) < MILESTONE_REACHED_SLACK) {
+                // Switch pivot
+                List<Vec> ends = getLinkEnds();
+                pivotPosition.headSet(ends.get(ends.size() - 1));
+                // Reset joint angles
+                float prevLinkAngleWithX = 0;
+                for (int i = ends.size() - 1; i > 0; --i) {
+                    Vec start = ends.get(i);
+                    Vec end = ends.get(i - 1);
+                    float angleWithX = (float) Math.atan2(end.get(1) - start.get(1), end.get(0) - start.get(0));
+                    float angleWithPrevLink = Angle.clamp_minusPI_plusPI(angleWithX - prevLinkAngleWithX);
+                    int jointVariable_iter = ends.size() - 1 - i;
+                    jointTuple.set(jointVariable_iter, angleWithPrevLink);
+                    prevLinkAngleWithX = angleWithX;
+                }
+                // Reverse lengths
+                Vec lengthsCopy = new Vec(lengths);
+                for (int i = lengthsCopy.getNumElements() - 1; i >= 0; i--) {
+                    lengths.set(lengthsCopy.getNumElements() - 1 - i, lengthsCopy.get(i));
+                }
+                nextMilestone++;
+                return;
+            }
+
+            // Distance from next milestone is significant => Update all joint variables such that free end moves to next milestone
+            Vec delta_jointTuple = RRIKSolver.jacobianTransposeStep(pivotPosition, lengths, jointTuple, path.get(nextMilestone));
+            delta_jointTuple.scaleInPlace(dt);
+            // If stuck in a singular configuration the give a little jerk
+            if (delta_jointTuple.norm() < JERK_THRESHOLD) {
+                for (int i = 0; i < delta_jointTuple.getNumElements(); i++) {
+                    delta_jointTuple.set(i, delta_jointTuple.get(i) + applet.random(0.5f));
+                }
+            }
+            jointTuple.plusInPlace(delta_jointTuple);
+        }
+    }
+
+    public void draw() {
+        // path
+        applet.stroke(1, 1, 0);
+        for (int i = 0; i < path.size() - 1; i++) {
+            Vec v1 = path.get(i);
+            Vec v2 = path.get(i + 1);
+            applet.line(0, v1.get(1), v1.get(0), 0, v2.get(1), v2.get(0));
+        }
+        applet.noStroke();
+        applet.fill(1, 1, 0);
+        for (Vec v : path) {
+            applet.pushMatrix();
+            applet.translate(0, v.get(1), v.get(0));
+            applet.box(1);
+            applet.popMatrix();
+        }
+        applet.noStroke();
+
+        // Pivot
+        applet.pushMatrix();
+        applet.noStroke();
+        applet.fill(0, 0, 1);
+        applet.translate(0, pivotPosition.get(1), pivotPosition.get(0));
+        applet.box(2);
+        applet.popMatrix();
+
+        // Links
+        Vec start = new Vec(pivotPosition);
+        Vec direction = new Vec(1f, 0f);
+        applet.noFill();
+        applet.stroke(1);
+        for (int i = 0; i < jointTuple.getNumElements(); i++) {
+            // Rotate
+            float theta = jointTuple.get(i);
+            direction = RotMat.of(theta).mult(direction);
+            // Translate
+            float length = lengths.get(i);
+            Vec end = start.plus(direction.scale(length));
+            // Draw link
+            applet.line(0, start.get(1), start.get(0), 0, end.get(1), end.get(0));
+            start = end;
+        }
+    }
+}

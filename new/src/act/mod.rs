5c3735b12ad0bf2f8dce136c882f4cc91b2b18fe
Yashasvi Sriram <yash.3997@gmail.com>
2020-11-23 00:47:08 -0600

    adds assert in NR::new() to check if each input q is b/w clamps, removes outdated reset()

--- new/src/act/mod.rs
+++ new/src/act/mod.rs
@@ -44,6 +44,8 @@ impl NR {
                 q_clamps[i].0 <= q_clamps[i].1,
                 format!("Invalid q clamp range argument. {:?}", q_clamps[i])
             );
+            assert!(q_clamps[i].0 <= qs[i], "Disobidient q arguement.");
+            assert!(qs[i] <= q_clamps[i].1, "Disobidient q arguement.");
         }
         assert!(thickness > 0.0, "Non-positive thickness argument");
         NR {
@@ -55,22 +57,6 @@ impl NR {
             delta_qs: Array1::<f32>::zeros((qs.len(),)),
             thickness: thickness,
         }
-    }
-
-    pub fn reset(&mut self, origin: Vec2, ls: &[f32], qs: &[f32]) {
-        assert_eq!(
-            ls.len(),
-            qs.len(),
-            "Unequal number of lengths and joint angles arguments."
-        );
-        for i in 0..ls.len() {
-            assert!(ls[i] > 0.0, "Non-positive length argument.");
-        }
-        self.n = ls.len();
-        self.origin = origin;
-        self.ls = arr1(ls);
-        self.qs = arr1(qs);
-        self.delta_qs = Array1::<f32>::zeros((qs.len(),));
     }
 
     pub fn thickness(&self) -> f32 {
